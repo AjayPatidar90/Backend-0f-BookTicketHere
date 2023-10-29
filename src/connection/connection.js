ac991a2eaaeedc9be75c75b3015a630cd5f0eb79
alma2023 <ajay.patidar90989@gmail.com>
2023-10-29 23:00:34 +0530

    modified connection

--- src/connection/connection.js
+++ src/connection/connection.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 
 
 
-const Database= mongoose.connect("mongodb+srv://ajaypatidar90989:ajay123@cluster0.lr0eb8u.mongodb.net/BookingDetail?retryWrites=true&w=majority").then((res)=>{
+const Database= mongoose.connect("mongodb+srv://ajaypatidar90989:@cluster0.lr0eb8u.mongodb.net/BookingDetail?retryWrites=true&w=majority").then((res)=>{
     console.log("Database is connect")
 }).catch((err)=>{
     console.log(err)
